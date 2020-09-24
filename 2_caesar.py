6a859e6ff601d9acb94a7f59f4af9e8e337cf262
Robin-Castellani <robin.castellani@alumni.unitn.it>
2020-09-24 11:57:03 +0200

    Feat: add first alternative solution to the second problem

--- 2_caesar.py
+++ 2_caesar.py
@@ -26,8 +26,27 @@ punctuation and whitespace remaining unchanged.
 """
 
 import string
+import functools
+import time
 
 
+def timer(func):
+    """
+    Simple decorator to print how much time a function requires
+    to run.
+    """
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"{func.__name__}: {elapsed_time * 10e6:0.0f} μseconds")
+        return value
+    return wrapper_timer
+
+
+@timer
 def cipher_caesar(text: str, n: int) -> str:
     """
     Encrypt or decrypt the letters in a string preserving all non
@@ -59,18 +78,56 @@ def cipher_caesar(text: str, n: int) -> str:
     return encrypted_text
 
 
+@timer
+def cipher_caesar_v2(text: str, n: int) -> str:
+    """
+    Second version of the using list comprehension. More concise.
+    Slower on single letters, faster on longer strings than V1.
+
+    Encrypt or decrypt the letters in a string preserving all non
+    letters characters (such as punctuation) according to the Caesar
+    method: shift the letters of ``n`` position.
+
+    :param text: string to be decrypted or encrypted.
+    :param n: number of position to shift the letters.
+    :return: decrypted/encrypted string.
+    """
+
+    # get the total number of lowercase letters
+    TOTAL_ASCII_LOWERCASE = len(string.ascii_lowercase)
+    # perform the encryption only on lowercase letters
+    # preserve the digits, punctuation and whitespaces
+    list_str_encrypt = [
+        string.ascii_lowercase[
+            string.ascii_lowercase.index(character) + n - TOTAL_ASCII_LOWERCASE
+        ]
+        if character in string.ascii_lowercase else character
+        for character in text
+    ]
+    encrypted_text = ''.join(list_str_encrypt)
+
+    return encrypted_text
+
+
 if __name__ == '__main__':
     for letter in string.ascii_lowercase:
         assert cipher_caesar(letter, 0) == letter
+        assert cipher_caesar_v2(letter, 0) == letter
 
     for dig in string.digits:
         assert cipher_caesar(dig, 10) == dig
+        assert cipher_caesar_v2(dig, 10) == dig
 
     for punct in string.punctuation:
         assert cipher_caesar(punct, 1) == punct
+        assert cipher_caesar_v2(punct, 1) == punct
 
     for whitespace in string.whitespace:
         assert cipher_caesar(whitespace, 7) == whitespace
+        assert cipher_caesar_v2(whitespace, 7) == whitespace
 
     assert cipher_caesar('rkgerzr nhgbzngvba vf njrfbzr!', 13) == \
+           'extreme automation is awesome!'
+
+    assert cipher_caesar_v2('rkgerzr nhgbzngvba vf njrfbzr!', 13) == \
            'extreme automation is awesome!'
